b3187f86b0211f5ac086f9c74b5a3dfa06cc482d
LoukasN <lounikus@gmail.com>
2023-08-30 21:32:09 +0300

    Dependancy changes

--- ConfigScripts/Dependancies.sh
+++ ConfigScripts/Dependancies.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 #installing dependancies for the project
-echo "Installing kitty, i3, i3status, i3clock-color, rofi, nitrogen, picom, neovim, setxbkmap"
-sudo apt install -y kitty i3 i3status i3lock-color rofi nitrogen picom neovim setxkbmap
+echo "Installing kitty, i3, i3status, rofi, nitrogen, picom, neovim"
+sudo apt install -y kitty i3 i3status rofi nitrogen picom neovim
  
