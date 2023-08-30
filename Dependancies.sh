ceff41ff2c767939363cbac5261569aa070afe6d
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-08-30 22:33:20 +0300

    Delete Dependancies.sh

--- Dependancies.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#installing dependancies for the project
-echo "Installing kitty, i3, i3status, rofi, nitrogen, picom, neovim, lightdm"
-sudo apt install -y kitty i3 i3status rofi nitrogen picom neovim lightdm
