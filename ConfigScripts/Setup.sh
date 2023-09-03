ba6373de247fa9cf6086d1be6b4356f246cc3695
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-09-03 12:46:58 +0300

    Update Setup.sh

--- ConfigScripts/Setup.sh
+++ ConfigScripts/Setup.sh
@@ -2,7 +2,8 @@
 
 #Moving files to Config folder
 echo "Copying folder to /home/$USER/.config"
-if [-d "/home/$USER/.config"]; then  
+if [-d "/home/$USER/.config"]; then
+  echo "Creating /home/$USER/.conf"
   mkdir /home/$USER/.config/
 fi
 cp -r ../* /home/$USER/.config/
