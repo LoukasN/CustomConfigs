ba7395199cad4d4fd6c4ffd929ae1fab792f997c
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-09-03 12:21:06 +0300

    if statement for .conf creation

--- ConfigScripts/Setup.sh
+++ ConfigScripts/Setup.sh
@@ -2,5 +2,7 @@
 
 #Moving files to Config folder
 echo "Copying folder to /home/$USER/.config"
-mkdir /home/$USER/.config/
+if [-d "/home/$USER/.config"]; then  
+  mkdir /home/$USER/.config/
+fi
 cp -r ../* /home/$USER/.config/
