43504a7d6b572e1a9160c56b547f76f2c942fd3c
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-09-03 12:45:31 +0300

    Update Dependancies.sh

--- ConfigScripts/Dependancies.sh
+++ ConfigScripts/Dependancies.sh
@@ -2,4 +2,4 @@
 
 #installing dependancies for the project
 echo "Installing kitty, i3, i3status, rofi, nitrogen, picom, neovim, lightdm"
-sudo apt install -y kitty i3 i3status rofi nitrogen picom neovim lightdm
+sudo pacman -S --noconfirm kitty i3 i3status rofi nitrogen picom neovim lightdm
