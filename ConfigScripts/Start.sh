73750b0e43921ffadeec3e55998ab9e65a3c18fa
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-09-03 12:48:22 +0300

    Update Start.sh

--- ConfigScripts/Start.sh
+++ ConfigScripts/Start.sh
@@ -1,7 +1,7 @@
 #! bin/bash
 
 echo "Downloading Dependancies...."
-bash Dependancies.sh
+bash /CustomConfigs/ConfigScripts/Dependancies.sh
 
 echo "Copying from $PWD to /home/$USER/.config/"
-bash Setup.sh
+bash /CUstomConfigs/ConfigScripts/Setup.sh
