3bba58fc5e481e0d9ff173b081dd82a0531e7fb4
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-09-05 13:56:04 +0300

    Update install.sh

--- ConfigScripts/install.sh
+++ ConfigScripts/install.sh
@@ -4,4 +4,4 @@ echo "Downloading Dependancies...."
 bash ~/CustomConfigs/ConfigScripts/Dependancies.sh
 
 echo "Copying from $PWD to /home/$USER/.config/"
-bash ~/CUstomConfigs/ConfigScripts/Setup.sh
+bash ~/CustomConfigs/ConfigScripts/Setup.sh
