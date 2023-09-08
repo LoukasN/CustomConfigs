a143a811e48d8bb40d6be7a0a7a21f2b319612c8
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-09-08 15:25:28 +0300

    Add files via upload

--- ConfigScripts/install.sh
+++ ConfigScripts/install.sh
@@ -1,7 +1,7 @@
 #! bin/bash
 
 echo "Downloading Dependancies...."
-bash ~/CustomConfigs/ConfigScripts/Dependancies.sh
+bash /CustomConfigs/ConfigScripts/Dependancies.sh
 
 echo "Copying from $PWD to /home/$USER/.config/"
-bash ~/CustomConfigs/ConfigScripts/Setup.sh
+bash /CUstomConfigs/ConfigScripts/Setup.sh
