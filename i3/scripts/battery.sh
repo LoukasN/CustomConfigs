7fa218350f871fac5804c4a504a3121bf40e9117
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2023-10-02 02:44:12 +0300

    Add files via upload

---
+++ i3/scripts/battery.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BAT=$(acpi -b | grep -E -o '[0-9][0-9]%')
+REMAINING=$(acpi -b | grep -E -o '[0-9][0-9]:[0-9][0-9]')
+
+if [[ $BAT > 30 ]]; then
+	echo "$BAT $REMAINING"
+elif [[ $BAT > 15 ]]; then
+	echo "$BAT $REMAINING"
+	echo
+	echo \#FFFF00 #YELLOW
+else
+	echo "$BAT $REMAINING"
+	echo
+	echo \#FF0000 #RED
+fi
+
+exit 0
---
+++ i3/scripts/date.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Time
+Time=$(date +%T)
+# Date with "correct" format
+Date=$(date +"%d/%m/%Y")
+
+echo "$Date  $Time"
+
