1de60ab56ff8d102efd905e98fd046100c6ceb78
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2024-01-30 15:17:06 +0200

    Add files via upload

--- nvim/lua/loukas/remap.lua
+++ nvim/lua/loukas/remap.lua
@@ -10,6 +10,3 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
---Terminal
-vim.keymap.set("n", "<F5>", "<cmd>terminal<CR>A")
-vim.keymap.set("t", "<F5>", "<C-d><CR>")
