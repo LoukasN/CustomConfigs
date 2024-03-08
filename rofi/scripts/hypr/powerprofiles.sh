8588dde4c4ef3e434cc588f86b9f6e7098c7f6cb
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2024-03-08 18:41:51 +0200

    Add files via upload

---
+++ rofi/scripts/hypr/powerprofiles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CurrentProfile=$(powerprofilesctl get)
+SelectedProfile=$CurrentProfile
+Performance="󰓅 Performance"
+Balanced="󰗑 Balanced"
+BatterySaving=" Power-Saver"
+
+SelectedProfile=$(echo "$Performance
+$Balanced
+$BatterySaving" | rofi -dmenu -i -theme "$HOME/.config/rofi/themes/Casual-Dark-PowerProfilesMenu.rasi")
+
+if [ "$SelectedProfile" == "$Performance" ]
+then
+	powerprofilesctl set performance
+	dunstify "Profile changed to $SelectedProfile"
+elif [ "$SelectedProfile" == "$Balanced" ]
+then
+	powerprofilesctl set balanced
+	dunstify "Profile changed to $SelectedProfile"
+elif [ "$SelectedProfile" == "$BatterySaving" ]
+then
+	powerprofilesctl set power-saver
+	echo "Hello"
+	dunstify "Profile changed to $SelectedProfile"
+fi
+
+exit
