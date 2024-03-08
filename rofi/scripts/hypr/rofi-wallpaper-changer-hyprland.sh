8dc75b16a644aced9ef694b1c4cba2c533f096cb
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2024-03-08 18:43:02 +0200

    Add files via upload

---
+++ rofi/scripts/hypr/rofi-wallpaper-changer-hyprland.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WallpaperDir="/home/$USER/Pictures/wallpapers/" # Wallpaper dir
+ConfigLocation="/home/$USER/.config/hypr/autostart.conf" # The config file to edit
+PrevWallpaper=$(cat "$ConfigLocation" | grep "swaybg" | awk -F '/' '{print $4}') # Get previous wallpapers lines from file)
+
+SelectPic(){
+	# Shows the wallpaper selection menu in rofi
+	Wallpaper=$(ls "$WallpaperDir" | rofi -dmenu -p "Select wallpaper")
+	if [[ $Wallpaper == "q" || $Wallpaper == "" ]]; then
+		exit
+	else
+		pkill swaybg
+		SetWallpaper
+	fi
+}
+
+SetWallpaper(){
+	sed -i -e "s/$PrevWallpaper/$Wallpaper/g" "$ConfigLocation"
+}
+
+SelectPic
+exit
