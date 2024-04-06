9a8e055e65d904646acd1769c5eeb7f1de73a43f
TreloFistiki <115584238+LoukasN@users.noreply.github.com>
2024-04-06 12:56:18 +0300

    Delete nvim directory

--- nvim/after/plugin/fugitive.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<leader>gg", '<cmd>:G <CR>', {desc = "Git menu"})
-vim.keymap.set("n", "<leader>gs", '<cmd>:Git status<CR>', {desc = "Git status"})
-vim.keymap.set("n", "<leader>gd", '<cmd>:Git diff<CR>', {desc = "Git differences"})
-vim.keymap.set("n", "<leader>gD", '<cmd>:Git difftool<CR>', {desc = "Git differences menu"})
