d8e6713f2efc050da30eaf71ce5d16b75ae3ee78
LoukasN <lounikus@gmail.com>
2024-08-02 21:31:34 +0300

    Sway logout from powermenu

---
+++ .config/rofi/scripts/sway/powermenu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+ 
+shutdown=" Poweroff"
+restart=" Restart"
+logout="󰍃 Logout"
+sleep="󰤄 Suspend"
+lock=" Lock"
+ 
+selected_option=$(echo "$shutdown
+$restart
+$logout
+$sleep
+$lock" | rofi -dmenu -i -p "Powermenu" -theme "$HOME/.config/rofi/themes/Casual-Dark-PowerMenu.rasi")
+
+if [ "$selected_option" == "$lock" ]
+then
+	loginctl lock-session
+	exit
+elif [ "$selected_option" == "$logout" ]
+then
+	swaymsg exit
+	exit
+elif [ "$selected_option" == "$shutdown" ]
+then
+	systemctl poweroff
+	exit
+elif [ "$selected_option" == "$restart" ]
+then
+	systemctl reboot
+	exit
+elif [ "$selected_option" == "$sleep" ]
+then
+	systemctl suspend
+	exit
+fi
