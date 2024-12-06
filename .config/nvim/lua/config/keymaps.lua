5beacde16b003c395a842e21089aa330ab9d4a59
LoukasN <lounikus@gmail.com>
2024-12-06 16:30:54 +0200

    left only keymaps

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- header key
+vim.g.mapleader = " "
+
+-- open file search
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "File search" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Vim movement in greek language
+vim.o.langmap =
+	"ΑA,ΒB,ΨC,ΔD,ΕE,ΦF,ΓG,ΗH,ΙI,ΞJ,ΚK,ΛL,ΜM,ΝN,ΟO,ΠP,QQ,ΡR,ΣS,ΤT,ΘU,ΩV,WW,ΧX,ΥY,ΖZ,αa,βb,ψc,δd,εe,φf,γg,ηh,ιi,ξj,κk,λl,μm,νn,οo,πp,qq,ρr,σs,τt,θu,ωv,ςw,χx,υy,ζz"
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
