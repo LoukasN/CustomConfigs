addc7f567240d7772972c939f289554e764ee5f5
LoukasN <lounikus@gmail.com>
2024-12-08 17:35:56 +0200

    new dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -9,6 +9,15 @@ return {
 			quickfile = { enabled = true },
 			statuscolumn = { enabled = true },
 			words = { enabled = true },
+			dashboard = {
+				enabled = true,
+				sections = {
+					{ section = "header" },
+					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				},
+			},
 		},
 	},
 }
