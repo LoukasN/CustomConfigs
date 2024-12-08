6ce1866ef727af6b62d58f6424e108204b88094f
LoukasN <lounikus@gmail.com>
2024-12-08 17:36:20 +0200

    markdown ensure and disable limit

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,20 @@
 return {
-	"nvim-treesitter/nvim-treesitter", 
+	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua", "c", "bash", "java"},
+			ensure_installed = { "lua", "c", "bash", "markdown" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
+			disable = function(lang, buf)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
 		})
-	end
+	end,
 }
-
