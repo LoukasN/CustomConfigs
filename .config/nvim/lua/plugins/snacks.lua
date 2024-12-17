21df51b5d83c79c112a11822e4de9cd050de9ff5
LoukasN <lounikus@gmail.com>
2024-12-17 19:48:58 +0200

    ?

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -13,9 +13,9 @@ return {
 				enabled = true,
 				sections = {
 					{ section = "header" },
-					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{ icon = "[] ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+					{ icon = "[] ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					{ icon = "[] ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 				},
 			},
 		},
